8cb6691beb1100683cc8a637f3bd555f5454007c
unknown <diesol42@gmail.com>
2019-05-02 11:37:05 -0700

    firebase working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,50 +10,53 @@ var config = {
 
   firebase.initializeApp(config);
 
-  firebase.database().ref().on("value", function(snapshot){
-      console.log(snapshot.val());
-  });
-
+//   firebase.database().ref().on("value", function(snapshot){
+//       console.log(snapshot.val());
+//   });
+var database = firebase.database();
 // tracking the inputs
-var trainName = "";
-var destination = "";
-var firstTrain = 0;
-var frequency = 0;
+// var trainName = "";
+// var destination = "";
+// var firstTrain = 0;
+// var frequency = 0;
 // will grab values inside the fields
 $("#add-train").on("click", function(event) {
     event.preventDefault();
     
-    trainName = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTrain = $("#time-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
+    var trainName = $("#name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrain);
+    console.log(frequency);
 // send data to firebase
-    firebase.database().ref().push({
-        Train: trainName,
-        Destination: destination,
-        FirstTrain: firstTrain,
-        Frequency: frequency,
-        dateAdded:firebase.database.ServerValue.TIMESTAMP
-
-    })
-
+    var newTrain = {
+        dTrain: trainName,
+        dDestination: destination,
+        dFirstTrain: firstTrain,
+        dFrequency: frequency,
+        // dateAdded:firebase.database.ServerValue.TIMESTAMP
+    };
+    database.ref().push(newTrain);
 })
 // place all our data from firebase into my div
 firebase.database().ref().on("child_added", function(snapshot){
-    $("#train-times").append("<p>" + snapshot.val().trainName+"</p>");
-    $("#train-times").append("<p>" + snapshot.val().destination+"</p>");
-    $("#train-times").append("<p>" + snapshot.val().firstTrain+"</p>");
-    $("#train-times").append("<p>" + snapshot.val().frequency+"</p>");
+    $("#train-times").append("<p>" + snapshot.val().dTrain+"</p>");
+    $("#train-times").append("<p>" + snapshot.val().dDestination+"</p>");
+    $("#train-times").append("<p>" + snapshot.val().dFirstTrain+"</p>");
+    $("#train-times").append("<p>" + snapshot.val().dFrequency+"</p>");
 })
 // grab information from firebase and display it onto the site
 // & snapshot will be used to do just that
 // orderByChild will order the properties by the new child_added value 
-firebase.database().ref().orderByChild("dateAdded").on("child_added", function(snapshot){
-    $("#name-display").html(snapshot.val().trainName);
-    $("#destination-display").html(snapshot.val().destination);
-    $("#time-display").html(snapshot.val().firstTrain);
-    $("#minutes-display").html(snapshot.val().frequency);
-});
+// firebase.database().ref().orderByChild("dateAdded").on("child_added", function(snapshot){
+//     $("#name-display").html(snapshot.val().trainName);
+//     $("#destination-display").html(snapshot.val().destination);
+//     $("#time-display").html(snapshot.val().firstTrain);
+//     $("#minutes-display").html(snapshot.val().frequency);
+// });
 
 // setting the times for the trains
 var tFrequency = 0;
